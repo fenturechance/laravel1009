633a985c00df98e67fdd41ec8b38bfaff7871472
fenturechance <fenturechance@gmail.com>
2017-10-09 18:46:17 +0800

    click event

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -2,20 +2,26 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
+                <label for="name">請輸入訊息</label>
                 <div class="input-group panel">
-                    <input id="password" type="password" class="form-control" name="password" placeholder="Additional Info" style="z-index:1">
-                    <span class="input-group-addon">Text</span>
+                    <input id="name" type="text" class="form-control" name="password" placeholder="輸入訊息" v-model="message">
+                    <span class="input-group-btn">
+                        <button class="btn btn-primary" type="button" @click="submitMessage">送出</button>
+                    </span>
                 </div>
                 <div class="panel panel-primary">
                     <div class="panel-heading">Example Component</div>
-                    <div class="panel-body">
+                    <div class="panel-body" v-for="item in messages">
                         <div class="media">
-                            <div class="media-left">
+                            <div class="media-left" v-if="item.username !=username ">
                                 <img src="http://fakeimg.pl/100x100/0088b2/fff/?text=A" class="media-object" style="width:60px">
                             </div>
                             <div class="media-body">
-                                <h4 class="media-heading">John Doe</h4>
-                                <p>Lorem ipsum...</p>
+                                <h4 class="media-heading">{{ item.username }}</h4>
+                                <p>{{ item.message  }}</p>
+                            </div>
+                            <div class="media-right"  v-if="item.username ==username ">
+                                <img src="http://fakeimg.pl/100x100/0088b2/fff/?text=A" class="media-object" style="width:60px">
                             </div>
                         </div>
                     </div>
@@ -32,6 +38,8 @@
         },
         data() {
             return {
+                username: 'Casper',
+                message:'',
                 messages: [{
                         username: 'Casper',
                         message: '哈囉你好'
@@ -41,6 +49,11 @@
                         message: '你也好'
                     }
                 ]
+            }
+        },
+        methods: {
+            submitMessage (){
+                console.log(this.message);
             }
         }
     }
