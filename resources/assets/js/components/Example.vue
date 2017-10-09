3325807648f2bbe0dba1a27b443a51666606668b
fenturechance <fenturechance@gmail.com>
2017-10-09 17:56:52 +0800

    dialog

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -2,6 +2,10 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
+                <div class="input-group panel">
+                    <input id="password" type="password" class="form-control" name="password" placeholder="Additional Info" style="z-index:1">
+                    <span class="input-group-addon">Text</span>
+                </div>
                 <div class="panel panel-primary">
                     <div class="panel-heading">Example Component</div>
                     <div class="panel-body">
